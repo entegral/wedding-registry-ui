bf220cd13d1e3c0518ee33510bea4f724a7c1795
entegral <entegral@protonmail.com>
2021-08-05 01:02:02 -0700

    update comment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FetchQuestions from "./Questions/FetchQuestions"
 function App({user}) {
   console.log('[App] user:', user)
 
-  // useEffect to fetch user data from dynamo and provide the data to 
+  // TODO useEffect to fetch user data from dynamo and provide the data to main app page manager
   return (
     <FetchQuestions user={user}/>
   )
